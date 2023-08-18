bcd2a1ab239f6bee7f95c1d45ff4e04425618d2a
Samrat Alam <samrat.alam@reddotdigitalit.com>
2023-08-18 19:21:12 +0600

    added

--- src/main/java/com/example/designpattern/DesignPatternInJavaApplication.java
+++ src/main/java/com/example/designpattern/DesignPatternInJavaApplication.java
@@ -8,6 +8,7 @@ public class DesignPatternInJavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternInJavaApplication.class, args);
+        System.out.println("Done.");
     }
 
 }
